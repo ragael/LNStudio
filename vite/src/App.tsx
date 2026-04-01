208ba7ed49d87a660524b670c0c6bb37762dd53a
Rafael Matos <ragael@gmail.com>
2026-04-01 18:48:41 -0300

    uso do hashrouter

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { BookOpen, Home, Settings } from 'lucide-react';
 import Dashboard from './pages/Dashboard';
 import FactoryWorkspace from './pages/FactoryWorkspace';
